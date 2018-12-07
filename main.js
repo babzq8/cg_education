2f3b1088cab6282e5f85795359593094df164d5f
babzq8 <brittneybarrick@gmail.com>
2018-12-06 22:11:49 -0600

    connected js file to all HTML files, created all variables

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('this will show up when the file is correctly linked!');
+//TEACHER PAGE DATA
+
+//variable declaration
+let teacherName = 'Liz Lemon';
+let departmentName = 'Comedy Writing';
+let ratingOne = '3.4';
+let ratingTwo = '5.0';
+let ratingThree = '4.0';
+let ratingSum = parseFloat(ratingOne) + parseFloat(ratingTwo) + parseFloat(ratingThree);
+let avgRating = (ratingSum / 3).toFixed(5);
+
+//STUDENT PAGE DATA
+
+//variable declaration
+let studentName = 'Aldous Snow';
+let studentEmail = 'snow@aldoussnow.com';
+let studentGPA = '3.2';
+let studentMajor = 'Music Performance';
+
+//COURSE PAGE DATA
+
+//variable declaration
+let semester = 'Spring 2019';
+let courseName = 'Improv Comedy';
