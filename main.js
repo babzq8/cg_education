4402e7c26c827446d4b8d5edebff8d2e647157d7
babzq8 <brittneybarrick@gmail.com>
2018-12-12 10:57:20 -0600

    completed courses department filter function

--- main.js
+++ main.js
@@ -22,12 +22,12 @@ const addTeacherRating = (ratingsArr, newRating) => {
 }
 
 //TEACHER RATING PROMPT
-let addtlRating = prompt('We would like for you to review. Please enter a rating between 0.0-5.0');
-
-if ((addtlRating >= 0) && (addtlRating <= 5)) {
-  addTeacherRating(teacherRating, addtlRating);
-  alert(`Thanks for you review! ${teacherName}\'s average rating is now .` );
-} else {prompt('We would like for you to review. Please enter a rating between 0.0-5.0');}
+// let addtlRating = prompt('We would like for you to review. Please enter a rating between 0.0-5.0');
+//
+// if ((addtlRating >= 0) && (addtlRating <= 5)) {
+//   addTeacherRating(teacherRating, addtlRating);
+//   alert(`Thanks for you review! ${teacherName}\'s average rating is now .` );
+// } else {prompt('We would like for you to review. Please enter a rating between 0.0-5.0');}
 
 
 
@@ -47,3 +47,12 @@ let courseName = 'Improv Comedy';
 let courseList = [['Improv Comedy', 'Comedy Writing'],['Acting 101','Comedy Writing'],['Singing 101','Comedy Writing'],['Dance 101','Comedy Writing'],['Basic HTML','Web Development'],['Intro to CSS','Web Development'],['Killing You with JavaScript','Web Development']];
 
 //COURSE FUNCTIONS
+const deptFilter = (courseArr, deptName) => {
+  let filtCourse = [];
+  for (let i=0; i< courseArr.length; i++) {
+    if (deptName === courseArr[i][1]) {
+      filtCourse.push(courseArr[i]);
+    }
+  }
+  return filtCourse;
+}
